d190dee27b41dc3dcd05b78b32c7c86e56223f50
Vera Fileyeva <veremei.vera@gmail.com>
2026-02-13 21:36:13 +0000

    /health route added

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -3,8 +3,8 @@ const express = require("express");
 import { Request, Response, NextFunction } from "express";
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.json("All good in here");
+router.get("/health", (req: Request, res: Response) => {
+  res.status(200).send("OK");
 });
 
 module.exports = router;
